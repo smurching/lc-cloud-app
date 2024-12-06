819a37f99e9de517a48f285f0a66a10b9c7dbe67
Sid Murching <sid.murching@databricks.com>
2024-12-06 00:01:14 -0800

    Use ChatOpenAI
    Signed-off-by: Sid Murching <sid.murching@databricks.com>

--- agent.py
+++ agent.py
@@ -1,9 +1,21 @@
 # agent.py
-from langchain_anthropic import ChatAnthropic
+from langchain_openai import ChatOpenAI
 from langchain_community.tools.tavily_search import TavilySearchResults
 from langgraph.prebuilt import create_react_agent
 
-model = ChatAnthropic(model="claude-3-5-sonnet-20240620")
+from langchain_openai import ChatOpenAI
+
+model = ChatOpenAI(
+    model="gpt-4o",
+    temperature=0,
+    max_tokens=None,
+    timeout=None,
+    max_retries=2,
+    # api_key="...",  # if you prefer to pass api key in directly instaed of using env vars
+    # base_url="...",
+    # organization="...",
+    # other params...
+)
 
 tools = [TavilySearchResults(max_results=2)]
 
